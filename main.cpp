f79590d329394900486022d560a017f43ac25b6d
Michal Nowicki <michalsminowicki@gmail.com>
2013-11-07 23:25:08 +0000

    Final version

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "handest_defs.h"
 #include "Grabber/kinect_grabber.h"
 #include "Core/Math/CMat44.h"
-#include "Kinematic/kinematic.h"
+#include "Kinematic/kinematic_liego.h"
 #define _USE_MATH_DEFINES
 #include <math.h>
 
@@ -43,47 +43,14 @@ int main()
 	}
 
 
-	/// Testing scenarios for FK
-	Finger::Pose *finger;
-	finger = new Finger::Pose();
-	Mat33 z;
-	z.m[0][0] = z.m[1][1] = z.m[2][2] = 1.0;
-	finger->pose.R = z;
-	Vec3 g;
-	finger->pose.p = g;
-	finger->pose.p.v[2] = 0.0;//10.0;
 
-	for (int i=0;i<3;i++)
-	{
-		for (int j=0;j<3;j++)
-		{
-			printf("%f ", finger->pose.R.m[i][j]);
-		}
-		printf("%f ", finger->pose.p.v[i]);
-		printf("\n");
-	}
+	// Before using, please fill:
+	// -Mat34 pose of the hand
+	// -Mat34 pose of the finger
+	// -lengths of each finger's links
+	ForwardKinematics *fk = new ForwardKinematicsLiego();
 
-	ForwardKinematics *fk;
-	fk = new ForwardKinematics();
-
-	Finger::Config config;
-	config.conf[0] = 0;
-	config.conf[1] = 0;
-	config.conf[2] = -45 * 3.14/180;
-	config.conf[3] = 90 * 3.14/180;
-	float len[3] = {1, 2, 2 };
-	fk->fingerFK(finger,config,len);
-
-	for (int i=0;i<3;i++)
-	{
-		printf("%f %f %f \n", finger->chain[i].pose.p.v[0], finger->chain[i].pose.p.v[1], finger->chain[i].pose.p.v[2]);
-	}
-
-	//std::cout<<std::endl<<fk->getEpsilon(Eigen::Vector3f::UnitZ(), Eigen::Vector3f(0, 0, 0))<<std::endl;
-
-	//Eigen::Matrix4f x = fk->matrixExp(fk->getEpsilon(Eigen::Vector3f::UnitZ(), Eigen::Vector3f(0, 0, 0)), config.conf[0], 15);
-
-	//std::cout<<std::endl<<x<<std::endl;
+	//fk->forward(hand, config)
 
 	return 0;
 }
